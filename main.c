85c1e986738efba6331f172ae7f186e5b995036f
weston-bailey <wdbailey87@gmail.com>
2021-07-07 18:53:44 -0700

    add position type

--- main.c
+++ main.c
@@ -2,23 +2,51 @@
 #include <ncurses.h>
 
 /* Game constants */
-#define PLAYER_CHAR "@"
-#define WALL_CHAR "|"
-#define FLOOR_CHAR "."
+#define CHAR_PLAYER "@"
+#define CHAR_WALL_Y "-"
+#define CHAR_WALL_X "|"
+#define CHAR_FLOOR "."
 
-#define QUIT_CHAR "q"
+#define KEY_QUIT 'q'
 
-typedef struct Player {
-  int xPosition;
-  int yPosition;
+/* GAME OBEJECT TYPE DEFINITIONS */
+
+typedef struct room Room;
+typedef struct player Player;
+typedef struct position Position;
+
+struct position {
+  int x;
+  int y;
+  // TILE_TYPE tile;
+};
+
+struct room {
+  Position position;
+  int height;
+  int width;
+  // Monster **monsters;
+  // Item **items;
+};
+
+struct player {
+  Position position;
   int health;
-} Player;
+  Room *room;
+};
 
 /* INTERFACES */
 
 int screen_setup (void);
-int map_setup (void);
-Player *player_setup(void); 
+Room **map_setup (void);
+Player *player_setup (void);
+int handle_input (int input, Player *user);
+int player_move (int y, int x, Player *user);
+int check_position(int newY, int newX, Player *user);
+
+/* ROOM FUNCTIONS */
+Room *create_room(int x, int y, int height, int width);
+int draw_room(Room *room);
 
 int main (void) 
 {
@@ -32,8 +60,8 @@ int main (void)
   user = player_setup();
 
   /* main game loop */
-  while ((ch = getch()) != 'q') {
-  
+  while ((ch = getch()) != KEY_QUIT) {
+    handle_input(ch, user); 
   }
   
   /* ends the ncurses session */
@@ -47,40 +75,94 @@ int screen_setup (void)
   /* startup ncurses */
   initscr();
   /* ncurses uses printw() instead of printf() */
-  printw("Hellow World");
+  /* printw("Hellow World"); */
   /* noecho prevents chars from kb input */
   noecho();
   /* you need to call refresh for some reason? -- TODO learn why */
   refresh();
-  return 0;
+  return 1;
 }
 
-/* initializes map */
-int map_setup (void)
+/* returns an array of rooms to setup map */
+Room **map_setup (void)
 {    
   /* ncurses (x_position, y_postion, string to print) -- moves to x/y and prints string */
-  mvprintw(13, 13, "--------");
-  mvprintw(14, 13, "|......|");
-  mvprintw(15, 13, "|......|");
-  mvprintw(16, 13, "|......|");
-  mvprintw(17, 13, "|......|");
-  mvprintw(18, 13, "--------");
+  /* array of rooms */
+  Room **rooms;
+  rooms = malloc(sizeof(Room*)*6);
+  // mvprintw(13, 13, "--------");
+  // mvprintw(14, 13, "|......|");
+  // mvprintw(15, 13, "|......|");
+  // mvprintw(16, 13, "|......|");
+  // mvprintw(17, 13, "|......|");
+  // mvprintw(18, 13, "--------");
 
-  mvprintw(2, 40, "--------");
-  mvprintw(3, 40, "|......|");
-  mvprintw(4, 40, "|......|");
-  mvprintw(5, 40, "|......|");
-  mvprintw(6, 40, "|......|");
-  mvprintw(7, 40, "--------");
+  rooms[0] = create_room(13, 13, 6, 8);
+  draw_room(rooms[0]);
 
-  mvprintw(10, 40, "------------");
-  mvprintw(11, 40, "|..........|");
-  mvprintw(12, 40, "|..........|");
-  mvprintw(13, 40, "|..........|");
-  mvprintw(14, 40, "|..........|");
-  mvprintw(15, 40, "------------");
+  // mvprintw(2, 40, "--------");
+  // mvprintw(3, 40, "|......|");
+  // mvprintw(4, 40, "|......|");
+  // mvprintw(5, 40, "|......|");
+  // mvprintw(6, 40, "|......|");
+  // mvprintw(7, 40, "--------");
 
-  return 0;
+  rooms[1] = create_room(40, 2, 6, 8);
+  draw_room(rooms[1]);
+
+  // mvprintw(10, 40, "------------");
+  // mvprintw(11, 40, "|..........|");
+  // mvprintw(12, 40, "|..........|");
+  // mvprintw(13, 40, "|..........|");
+  // mvprintw(14, 40, "|..........|");
+  // mvprintw(15, 40, "------------");
+
+  rooms[2] = create_room(40, 10, 6, 12);
+  draw_room(rooms[2]);
+
+  return rooms;
+}
+
+
+Room *create_room(int x, int y, int height, int width)
+{
+  /* declare a new room */
+  Room *newRoom;
+  newRoom = malloc(sizeof(Room));
+
+  newRoom->position.x = x;
+  newRoom->position.y = y;
+  newRoom->height = height;
+  newRoom->width = width;
+
+  return newRoom;
+}
+
+int draw_room(Room *room)
+{
+  /* first draw top and bottom */
+  int i;
+  for (i = room->position.x; i < room->position.x + room->width; i++) {
+    /* top of the room */
+    mvprintw(room->position.y, i, CHAR_WALL_Y);
+    /*bottom of the room */
+    mvprintw(room->position.y + room->height - 1, i, CHAR_WALL_Y);
+  }
+
+  /* draw floors and side walls */
+  int j; 
+  int k;
+  for (j = room->position.y + 1; j < room->position.y + room->height - 1; j++) { 
+    /* draw side walls */
+    mvprintw(j, room->position.x, CHAR_WALL_X);
+    mvprintw(j, room->position.x + room->width - 1, CHAR_WALL_X);
+    /* draw floors */
+    for (k = room->position.x + 1; k < room->position.x + room->width - 1; k++) {
+      mvprintw(j, k, CHAR_FLOOR);
+    }
+  }
+
+  return 1;
 }
 
 /* creates a new player object */
@@ -89,11 +171,11 @@ Player *player_setup (void)
   Player *newPlayer;
   newPlayer = malloc(sizeof(Player));
 
-  newPlayer->xPosition = 14;
-  newPlayer->yPosition = 14;
+  newPlayer->position.x = 14;
+  newPlayer->position.y = 14;
   newPlayer->health = 20;
 
-  mvprintw(newPlayer->yPosition, newPlayer->xPosition, PLAYER_CHAR);
+  mvprintw(newPlayer->position.y, newPlayer->position.x, CHAR_PLAYER);
 
   /* move the cursor without printing */
   move(14, 14);
@@ -101,9 +183,78 @@ Player *player_setup (void)
   return newPlayer;
 }
 
+int handle_input (int input, Player *user) 
+{
+  int newY;
+  int newX;
+  switch (input)
+  {
+    /* move up */
+    case 'w':
+    case 'W':
+      newY = user->position.y - 1;
+      newX = user->position.x;
+      break;
+    
+    /* move down */
+    case 's':
+    case 'S':
+      newY = user->position.y + 1;
+      newX = user->position.x;
+      break;
 
+    /* move right */
+    case 'a':
+    case 'A':
+      newY = user->position.y;
+      newX = user->position.x - 1;
+      break;
 
+    /* move left */
+    case 'd':
+    case 'D':
+      newY = user->position.y;
+      newX = user->position.x + 1;
+      break;
 
+    default:
+      break;
+  }
+
+  check_position(newY, newX, user);
+
+  return 1;
+}
+
+/* check what is at the next position  where the user is trying to move */
+int check_position (int newY, int newX, Player *user)
+{
+  /* the space the user is trying to move to */
+  int space = mvinch(newY, newX);
+  /* mvinch(y, x) returns the character at a specifc y x position */
+  switch (mvinch(newY, newX))
+  {
+    case '.':
+      player_move (newY, newX, user);
+      break;
+    default:
+      move(user->position.y, user->position.x);
+      break;
+  }
+
+  return 0;
+}
+
+int player_move (int y, int x, Player *user)
+{
+  mvprintw(user->position.y, user->position.x, CHAR_FLOOR);
+
+  user->position.y = y;
+  user->position.x = x;
+
+  mvprintw(user->position.y, user->position.x, CHAR_PLAYER);
+  return 1;
+}
 
 
 
